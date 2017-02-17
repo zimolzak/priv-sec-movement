0cde1954803e2bd6b06982daf33b0fb45d632d9e
Andrew Zimolzak <andyzimolzak@gmail.com>
2017-02-17 17:00:17 -0500

    Fix censoring.

--- censor.pl
+++ censor.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 use strict;
 while(<>){
-    s/Warning: rude language in this list./Note: some rude language has been censored./g;
+    s/Warning: there is rude language in this list./Note: some rude language has been censored./g;
     s/fuck/f---k/g;
     s/asshole/a---hole/g;
     s/pussy/pu---y/g;
